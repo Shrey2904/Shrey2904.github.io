565e5cef211486180bccdca9d0acdc17f4cdc2e0
Shrey Tanna <shreytanna@yahoo.in>
2023-10-31 18:02:24 -0400

    changes

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Layout />} >
+        <Route basename={process.env.PUBLIC_URL} path="/" element={<Layout />} >
 
           <Route index element={<Home />}/>
           <Route path="about" element={<About />} />
