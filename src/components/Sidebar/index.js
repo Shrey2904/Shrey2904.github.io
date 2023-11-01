facb4cc35eed0fb916f0404322679ac01097da27
Shrey Tanna <shreytanna@yahoo.in>
2023-11-01 03:26:22 -0400

    responsive changes

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -3,47 +3,68 @@ import { Link, NavLink } from 'react-router-dom'
 import Initial from '../../assets/images/initial.png'
 import FullName from '../../assets/images/name.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons' 
+import { faBars, faClose, faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons' 
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { useState } from 'react'
 
-const Sidebar = () => (
-    <div className='nav-bar'>
-        <Link className='logo' to='/'>
-            <img src={Initial} alt="logo"/>
-            <img className='fullname' src={FullName} alt="Shrey Tanna"/>
-        </Link>
+const Sidebar = () => {
 
-        <nav>
-            <NavLink exact='true' activeclassname='active' to='/'>
-                <FontAwesomeIcon icon={faHome} color='#4d4d4e'/>
-            </NavLink>
-            
-            <NavLink exact='true' activeclassname='active' className='about' to='/about'>
-                <FontAwesomeIcon icon={faUser} color='#4d4d4e'/>
-            </NavLink>
+const [showNav, setShowNav] = useState(false);
 
-            <NavLink exact='true' activeclassname='active' className='contact' to='/contact'>
-                <FontAwesomeIcon icon={faEnvelope} color='#4d4d4e'/>
-            </NavLink>
-        </nav>
+    return (
+        <div className='nav-bar'>
+            <Link className='logo' to='/'>
+                <img src={Initial} alt="logo"/>
+                <img className='fullname' src={FullName} alt="Shrey Tanna"/>
+            </Link>
+
+            <nav className={showNav ? 'mobile-show' : ''}>
+                <NavLink onClick={() => setShowNav(false)} exact='true' activeclassname='active' to='/'>
+                    <FontAwesomeIcon icon={faHome} color='#4d4d4e'/>
+                </NavLink>
+                
+                <NavLink onClick={() => setShowNav(false)} exact='true' activeclassname='active' className='about' to='/about'>
+                    <FontAwesomeIcon icon={faUser} color='#4d4d4e'/>
+                </NavLink>
+
+                <NavLink onClick={() => setShowNav(false)} exact='true' activeclassname='active' className='contact' to='/contact'>
+                    <FontAwesomeIcon icon={faEnvelope} color='#4d4d4e'/>
+                </NavLink>
+
+                <FontAwesomeIcon 
+                    onClick={() => setShowNav(false)}
+                    icon={faClose}
+                    color='#ffd700'
+                    size='3x'
+                    className='close-icon'
+                />
+            </nav>
 
 
-        <ul>
-            <li>
-                <a target="_blank"  rel = 'noreferrer' href="https://www.linkedin.com/in/shrey2904">
-                    <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e"/>
-                </a>
-            </li>
+            <ul>
+                <li>
+                    <a target="_blank"  rel = 'noreferrer' href="https://www.linkedin.com/in/shrey2904">
+                        <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e"/>
+                    </a>
+                </li>
 
 
-            <li>
-                <a target="_blank"  rel = 'noreferrer' href="https://github.com/Shrey2904">
-                    <FontAwesomeIcon icon={faGithub} color="#4d4d4e"/>
-                </a>
-            </li>
-        </ul> 
+                <li>
+                    <a target="_blank"  rel = 'noreferrer' href="https://github.com/Shrey2904">
+                        <FontAwesomeIcon icon={faGithub} color="#4d4d4e"/>
+                    </a>
+                </li>
+            </ul>
+            <FontAwesomeIcon
+                onClick={() => setShowNav(true)}
+                icon={faBars}
+                color='#ffd700'
+                size='3x'
+                className='hamburger-icon'
+            />
 
-    </div>
-)
+        </div>
+    )
+}
 
 export default Sidebar
